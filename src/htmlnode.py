65591fa1d5e1014a14c3e76d9fae5814934eef1e
Andyc731 <128937683+Andyc731@users.noreply.github.com>
2024-03-06 10:35:46 -0700

    implement LeafNode and ParentNode

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -15,14 +15,37 @@ class HTMLNode:
         new_string = ""
         for key, value in self.props.items():
             new_string += f'{key}="{value}" '
-        return new_string
+        return new_string.strip(' ')
 
     def __repr__(self):
         return f'HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})'
 
 
 class LeafNode(HTMLNode):
-    def __init__(self, tag, value, props):
-        self.tag = tag
-        self.value = value
-        self.props = props
+    def __init__(self, value, tag=None, props=None):
+        super().__init__(tag=tag, value=value, props=props)
+
+    def to_html(self):
+        if not self.tag:
+            return f'{self.value}'
+        if not self.props:
+            return f'<{self.tag}>{self.value}</{self.tag}>'
+        return f'<{self.tag} {self.props_to_html()}>{self.value}</{self.tag}>'
+
+
+class ParentNode(HTMLNode):
+    def __init__(self, children, tag=None):
+        super().__init__(children=children, tag=tag)
+
+    def to_html(self):
+        if not self.tag:
+            raise ValueError('No tag found')
+        if not self.children:
+            raise ValueError('No children found')
+
+        new_string = ''
+
+        for node in self.children:
+            new_string += node.to_html()
+
+        return f'<{self.tag}>{new_string}</{self.tag}>'
