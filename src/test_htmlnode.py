740de9ffb0cef7ad53bc56012e8ccfd8c8f6bc40
Andyc731 <128937683+Andyc731@users.noreply.github.com>
2024-03-06 10:36:13 -0700

    add tests for LeafNode and ParentNode

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,18 +1,37 @@
 
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode, ParentNode
+
 
 class TestHTMLNode(unittest.TestCase):
     def test_props_to_html(self):
-        node = HTMLNode("h1", "blah", '', { 'key1': 'value1', 'key2': 'value2'})
-        node2 = HTMLNode("h1", "blah", '')
-        self.assertEqual( node.props_to_html(), 'key1="value1" key2="value2" ')
-        self.assertEqual( node2.props_to_html(), '')
+        node = HTMLNode(tag="h1", value="blah", props={
+                        'key1': 'value1', 'key2': 'value2'})
+        node2 = HTMLNode(tag="h1", value="blah",)
+        self.assertEqual(node.props_to_html(), 'key1="value1" key2="value2"')
+        self.assertEqual(node2.props_to_html(), '')
 
     def test_repr(self):
-        node = HTMLNode("h1", "blah", '', { 'key1': 'value1', 'key2': 'value2'})
-        self.assertEqual(repr(node), "HTMLNode(h1, blah, , {'key1': 'value1', 'key2': 'value2'})" )
+        node = HTMLNode("h1", "blah", '', {'key1': 'value1', 'key2': 'value2'})
+        self.assertEqual(
+            repr(node), "HTMLNode(h1, blah, , {'key1': 'value1', 'key2': 'value2'})")
+
+    def test_leafnode(self):
+        node = LeafNode(tag='h1', value='Heading 1', props={
+                        'key1': 'value1', 'key2': 'value2'})
+        self.assertEqual(
+            node.to_html(), '<h1 key1="value1" key2="value2">Heading 1</h1>')
+
+    def test_parentnode(self):
+        node = ParentNode(tag='h1', children=[ParentNode(
+            tag='h2', children=[LeafNode("Normal Text"), LeafNode('Second Node', tag='p')])])
+        node2 = ParentNode(tag='div', children=[LeafNode(value='Normal Text', tag='p'), LeafNode(
+            'Second Text', tag='a', props={'href': 'https://github.com'})])
+        self.assertEqual(
+            node.to_html(), '<h1><h2>Normal Text<p>Second Node</p></h2></h1>')
+        self.assertEqual(
+            node2.to_html(), '<div><p>Normal Text</p><a href="https://github.com">Second Text</a></div>')
 
 
 if __name__ == "__main__":
