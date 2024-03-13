e7b62b66a6d49de9a383f0bcbbac12304a372fef
Andy <andychc731@gmail.com>
2024-03-13 11:57:57 -0600

    use generate pages function

--- src/main.py
+++ src/main.py
@@ -2,9 +2,14 @@ import os
 import shutil
 
 from copystatic import copystatic
+from generate_page import generate_pages_recursive
 
 dir_path_static = "./static"
 dir_path_public = "./public"
+
+path_markdown = "./content/"
+path_template_html = "./template.html"
+path_dest = "./public/"
 
 
 def main():
@@ -12,6 +17,8 @@ def main():
         shutil.rmtree(dir_path_public)
 
     copystatic(dir_path_static, dir_path_public)
+
+    generate_pages_recursive(path_markdown, path_template_html, path_dest)
 
 
 main()
