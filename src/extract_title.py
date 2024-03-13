2156e8e314a32d2cee0c0b699fb0c7c8dc377be8
Andy <andychc731@gmail.com>
2024-03-12 18:16:15 -0600

    extract title from markdown

---
+++ src/extract_title.py
@@ -0,0 +1,16 @@
+from block_markdown import markdown_to_blocks
+
+
+def extract_title(markdown):
+    blocks = markdown_to_blocks(markdown)
+
+    for block in blocks:
+        count = 0
+        block_copy = block
+        while block_copy.startswith("#"):
+            block_copy = block_copy[1:]
+            count += 1
+
+        if count == 1:
+            return block
+    raise Exception("no h1 header")
